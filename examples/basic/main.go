ff08420c1b15108a80e63c7e0f519266db9edc24
tester305 <sezersamettugra@gmail.com>
2025-10-25 22:40:52 +0300

    Update main.go

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -1,12 +1,20 @@
 package main
 
-import webview "github.com/tester305/webview_go"
+import (
+    webview "github.com/tester305/webview_go"
+    "log"
+)
 
 func main() {
-	w := webview.New(false)
-	defer w.Destroy()
-	w.SetTitle("Basic Example")
-	w.SetSize(480, 320, webview.HintNone)
-	w.SetHtml("Thanks for using webview!")
-	w.Run()
+    w := webview.New(false)
+    if w == nil {
+        log.Fatal("Failed to initialize webview: returned nil")
+    }
+    defer w.Destroy()
+
+    w.SetTitle("Basic Example")
+    w.SetSize(480, 320, webview.HintNone)
+    w.SetHtml("If you see this that means the webview succeeded, you can use me now.")
+    w.Run()
 }
+
