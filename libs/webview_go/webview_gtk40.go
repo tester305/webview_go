07957c0790cb843eceb5d9dd8b0f255a56a03186
tester305 <sezersamettugra@gmail.com>
2025-10-28 21:05:31 +0300

    Add dual GTK build support with gtk40 and gtk41 build tags

---
+++ libs/webview_go/webview_gtk40.go
@@ -0,0 +1,1 @@
+//go:build gtk40\n// +build gtk40\n\npackage webview\n\n/*\n#cgo pkg-config: gtk+-3.0 webkit2gtk-4.0\n*/\nimport "C"
---
+++ libs/webview_go/webview_gtk41.go
@@ -0,0 +1,1 @@
+//go:build gtk41\n// +build gtk41\n\npackage webview\n\n/*\n#cgo pkg-config: gtk+-3.0 webkit2gtk-4.1\n*/\nimport "C"
