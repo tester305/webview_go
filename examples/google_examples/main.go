ccfabcbda5030009313cdb0da86ca8fd7cb6e448
tester305 <sezersamettugra@gmail.com>
2025-11-06 13:47:15 +0300

    Update main.go

--- examples/google_examples/main.go
+++ examples/google_examples/main.go
@@ -5,7 +5,7 @@ import webview "github.com/tester305/webview_go"
 func main() {
     w := webview.New(false)
     defer w.Destroy()
-    w.SetTitle("Google Viewer")
+    w.SetTitle("Google")
     w.SetSize(480, 320, webview.HintNone)
 
     // Navigate to Google instead of using embedded HTML
