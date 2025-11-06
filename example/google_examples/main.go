080ef1ffdab72e500ef790881587b420f7f3429c
tester305 <sezersamettugra@gmail.com>
2025-11-06 13:27:04 +0300

    Create main.go

---
+++ example/google_examples/main.go
@@ -0,0 +1,15 @@
+package main
+
+import webview "github.com/tester305/webview_go"
+
+func main() {
+    w := webview.New(false)
+    defer w.Destroy()
+    w.SetTitle("Google Viewer")
+    w.SetSize(480, 320, webview.HintNone)
+
+    // Navigate to Google instead of using embedded HTML
+    w.Navigate("https://www.google.com")
+
+    w.Run()
+}
